558fd976d0a447799e71de232fd94389d1d89db3
Will S <fevenkitsune@gmail.com>
2021-08-26 16:22:07 -0400

    + Framework for the dumb control logic

---
+++ examples/dumb_control_motor_demo/code.py
@@ -0,0 +1,10 @@
+"""
+TODO:
+How the logic will work:
+Set point and start timer.
+While timer is under 5 seconds:
+    If set point is higher than current point, go forwards.
+    If set point is lower than current point, go backwards.
+Set throttle to zero once timer has expired.
+Wait for new set point.
+"""
